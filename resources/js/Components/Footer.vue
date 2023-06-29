4c10cd39c845bcb3a06324b4080b6a55fd281840
Stefan Jeremic <stefan.jeremic@outlook.com>
2023-06-29 15:58:20 +0200

    footer added

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+  <footer>
+    <div class="bg-w-gray">
+      <div class="bg-white p-32 rounded-tl-[10rem]">
+        <div class="container">
+          <div class="grid grid-cols-4 gap-8">
+            <div>
+              <h5 class="text-xl font-prompt-medium mb-6">SHOP</h5>
+
+              <p class="font-prompt-regular mb-3">Shop All Men's</p>
+              <p class="font-prompt-regular mb-3">Shop All Women's</p>
+              <p class="font-prompt-regular mb-3">Gift Card</p>
+              <p class="font-prompt-regular mb-3">Mens Kits</p>
+              <p class="font-prompt-regular mb-3">Womens Kits</p>
+              <p class="font-prompt-regular mb-3">VIP Membership</p>
+            </div>
+
+            <div>
+              <h5 class="text-xl font-prompt-medium mb-6">KIMBEL</h5>
+
+              <p class="font-prompt-regular mb-3">Our Story</p>
+              <p class="font-prompt-regular mb-3">Blog</p>
+              <p class="font-prompt-regular mb-3">Rewards</p>
+              <p class="font-prompt-regular mb-3">Careers</p>
+              <p class="font-prompt-regular mb-3">Become a Retailer</p>
+              <p class="font-prompt-regular mb-3">CUTS Marketplace</p>
+            </div>
+
+            <div>
+              <h5 class="text-xl font-prompt-medium mb-6">HELP</h5>
+
+              <p class="font-prompt-regular mb-3">FAQ</p>
+              <p class="font-prompt-regular mb-3">My Account</p>
+              <p class="font-prompt-regular mb-3">Track Order</p>
+              <p class="font-prompt-regular mb-3">Returns & Exchanges</p>
+              <p class="font-prompt-regular mb-3">Shipping</p>
+              <p class="font-prompt-regular mb-3">Protection</p>
+              <p class="font-prompt-regular">Policy Size</p>
+            </div>
+
+            <div>
+              <h5 class="text-xl font-prompt-medium mb-6">CONTACT</h5>
+
+              <p class="font-prompt-regular mb-3">Fit</p>
+              <p class="font-prompt-regular mb-3">Contact Us</p>
+              <p class="font-prompt-regular mb-3">Accessibility</p>
+              <p class="font-prompt-regular mb-3">Returns & Exchanges</p>
+              <p class="font-prompt-regular mb-3">Shipping</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <div class="bg-w-gray-1 rounded-t-[6rem] p-6">
+    <div class="container">
+      <div class="flex items-between">
+        <div class="inline-block">
+          <img
+            src="https://assets.website-files.com/637614467ad6ef67f0af2c24/637ded1cd93444319c6845cc_logo2.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
